c5838f73d2566f88c61bc2ab97d5f806252cc481
scabi0sa <scabiosa20425@gmail.com>
2025-12-12 19:11:08 +0900

    add: [WIP]DB実装

--- src/db/model.ts
+++ src/db/model.ts
@@ -1,5 +1,5 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
-import { sql } from 'drizzle-orm';
+import { sqliteTable, AnySQLiteColumn, text, integer ,unique } from 'drizzle-orm/sqlite-core'
+import { sql, } from 'drizzle-orm';
 
 export const users = sqliteTable('users', {
   id: text('id').primaryKey(),
@@ -19,3 +19,61 @@ export const users = sqliteTable('users', {
   createdAt: integer('created_at', { mode: 'timestamp' }).notNull().default(sql`(CURRENT_TIMESTAMP)`),
   updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull().default(sql`(CURRENT_TIMESTAMP)`),
 })
+
+//snsリンク
+export const snsUrl = sqliteTable('sns_url', {
+  id: text('id').primaryKey().$defaultFn(()=>crypto.randomUUID()),
+  url: text('url').notNull(),
+  userId: text('user_id').notNull().references(() => users.id),
+},(t)=>[
+  unique().on(t.userId, t.url),
+]);
+
+//コミュニティ
+export const communities = sqliteTable('communities', {
+  id: text('id').primaryKey(),
+  name: text('name'),
+  iconUrl:text('icon_url'),
+  description: text('description'),
+  createdAt: integer('created_at', { mode: 'timestamp' }).notNull().default(sql`(CURRENT_TIMESTAMP)`),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull().default(sql`(CURRENT_TIMESTAMP)`),
+
+})
+
+export const lists = sqliteTable('lists', {
+  id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+  name: text('name').notNull(),
+  description: text('description'),
+  thumbnailUrl: text('thumbnail_url'),
+  userId: text('user_id').notNull().references(() => users.id),
+  createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+});
+
+
+export const items = sqliteTable('items', {
+  id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+  title: text('title').notNull(),
+  author: text('author'),
+  url: text('url'),
+  imageUrl: text('image_url'),
+  genreId: text('genre_id').references(() => genre.id),
+  createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+});
+
+export const genre = sqliteTable('genre', {
+  id: text('id').primaryKey(),
+  name: text('name').notNull().unique(),
+});
+
+
+export const threads = sqliteTable('threads', {
+  id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+  userId: text('user_id').notNull().references(() => users.id),
+  itemId: text('item_id').notNull().references(() => items.id),
+  // 自己参照（親コメントID）
+  replyId: text('reply_id').references((): AnySQLiteColumn => threads.id),
+  text: text('text').notNull(),
+  createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+});
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,11 @@ import type { Hono } from 'hono'
 import { testRouter } from './test'
 import { authApp } from './auth'
 import { searchRouter } from './search'
+import { comApp } from './community'
 
 export const bindRoutes = (app: Hono) => {
     app.route('/test', testRouter)
     app.route('/auth', authApp)
     app.route('/search', searchRouter)
+    app.route('/community', comApp)
 }
