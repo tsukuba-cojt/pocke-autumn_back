788df931c6d3d98aecbd966c49ed8afca081757c
scabi0sa <scabiosa20425@gmail.com>
2025-12-12 19:11:08 +0900

    add: [WIP]DB実装

---
+++ src/router/community.ts
@@ -0,0 +1,20 @@
+import { Hono } from 'hono'
+import { AppEnv } from '../middleware/db'
+import { jwt } from 'hono/jwt'
+import { users } from '../db/model'
+import { eq, or, and } from 'drizzle-orm'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+export const comApp = new Hono<AppEnv>()
+
+//認証
+comApp.use('/', (c,next)=>{
+  const jwtMiddleware = jwt({secret: c.env.JWT_SECRET,})
+  return jwtMiddleware(c,next)
+})
+
+comApp.get('/',  async (c) => {
+    
+  }
+)
