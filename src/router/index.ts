437aa7c29d91b35109fece058b6a6b35e73fe455
Utsugi0101 <soraki0101@gmail.com>
2025-12-19 15:55:39 +0900

    リスト作成関数を作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,11 @@ import type { Hono } from 'hono'
 import { testRouter } from './test'
 import { authApp } from './auth'
 import { searchRouter } from './search'
-import { meApp } from './me'
-import { comApp } from './community'
 import { listRouter } from './list'
 
 export const bindRoutes = (app: Hono) => {
     app.route('/test', testRouter)
     app.route('/auth', authApp)
     app.route('/search', searchRouter)
-    app.route('/me', meApp)
-    app.route('/community', comApp)
     app.route('/list', listRouter)
 }
