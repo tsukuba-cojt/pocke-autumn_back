c09a671b5524b9c115bc29cea563547839c38a76
Utsugi0101 <soraki0101@gmail.com>
2026-01-07 13:50:19 +0900

    sqlをmainに合わせた

--- drizzle/0003_watery_thunderbolt_ross.sql
+++ drizzle/0003_watery_thunderbolt_ross.sql
@@ -2,7 +2,7 @@ CREATE TABLE `community_lists` (
 	`id` text PRIMARY KEY NOT NULL,
 	`community_id` text NOT NULL,
 	`lists_id` text NOT NULL,
-	`created_at` integer DEFAULT (strftime('%s','now')),
+	`created_at` integer DEFAULT strftime('%s','now'),
 	FOREIGN KEY (`community_id`) REFERENCES `communities`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`lists_id`) REFERENCES `lists`(`id`) ON UPDATE no action ON DELETE no action
 );
@@ -21,7 +21,7 @@ CREATE TABLE `community_members` (
 	`user_id` text NOT NULL,
 	`community_id` text NOT NULL,
 	`authority` text,
-	`joined_at` integer DEFAULT (strftime('%s','now')),
+	`joined_at` integer DEFAULT strftime('%s','now'),
 	PRIMARY KEY(`user_id`, `community_id`),
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`community_id`) REFERENCES `communities`(`id`) ON UPDATE no action ON DELETE no action
@@ -30,7 +30,7 @@ CREATE TABLE `community_members` (
 CREATE TABLE `fav_list` (
 	`list_item_id` text NOT NULL,
 	`user_id` text NOT NULL,
-	`created_at` integer DEFAULT (strftime('%s','now')),
+	`created_at` integer DEFAULT strftime('%s','now'),
 	FOREIGN KEY (`list_item_id`) REFERENCES `list_items`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
@@ -41,7 +41,7 @@ CREATE TABLE `list_items` (
 	`item_id` text NOT NULL,
 	`lists_id` text NOT NULL,
 	`user_id` text NOT NULL,
-	`created_at` integer DEFAULT (strftime('%s','now')),
+	`created_at` integer DEFAULT strftime('%s','now'),
 	FOREIGN KEY (`item_id`) REFERENCES `items`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`lists_id`) REFERENCES `lists`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
@@ -51,7 +51,7 @@ CREATE UNIQUE INDEX `list_items_item_id_lists_id_unique` ON `list_items` (`item_
 CREATE TABLE `me_too` (
 	`list_item_id` text NOT NULL,
 	`user_id` text NOT NULL,
-	`created_at` integer DEFAULT (strftime('%s','now')),
+	`created_at` integer DEFAULT strftime('%s','now'),
 	FOREIGN KEY (`list_item_id`) REFERENCES `list_items`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
@@ -76,3 +76,38 @@ CREATE TABLE `threads` (
 	FOREIGN KEY (`item_id`) REFERENCES `list_items`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`reply_id`) REFERENCES `threads`(`id`) ON UPDATE no action ON DELETE no action
 );
+--> statement-breakpoint
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`email` text NOT NULL,
+	`password_hash` text,
+	`auth_provider` text,
+	`auth_id` text,
+	`username` text NOT NULL,
+	`icon_url` text,
+	`description` text,
+	`created_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
+	`updated_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL
+);
+--> statement-breakpoint
+INSERT INTO `__new_users`("id", "email", "password_hash", "auth_provider", "auth_id", "username", "icon_url", "description", "created_at", "updated_at") SELECT "id", "email", "password_hash", "auth_provider", "auth_id", "username", "icon_url", "description", "created_at", "updated_at" FROM `users`;--> statement-breakpoint
+DROP TABLE `users`;--> statement-breakpoint
+ALTER TABLE `__new_users` RENAME TO `users`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
+CREATE UNIQUE INDEX `users_auth_id_unique` ON `users` (`auth_id`);--> statement-breakpoint
+CREATE TABLE `__new_lists` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`description` text,
+	`thumbnail_url` text,
+	`user_id` text NOT NULL,
+	`created_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
+	`updated_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+INSERT INTO `__new_lists`("id", "name", "description", "thumbnail_url", "user_id", "created_at", "updated_at") SELECT "id", "name", "description", "thumbnail_url", "user_id", "created_at", "updated_at" FROM `lists`;--> statement-breakpoint
+DROP TABLE `lists`;--> statement-breakpoint
+ALTER TABLE `__new_lists` RENAME TO `lists`;
